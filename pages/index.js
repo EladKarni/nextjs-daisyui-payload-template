98ee0aa5348f8038192b8b00a81575ca3404a5b1
Elad <39969481+EladKarni@users.noreply.github.com>
2019-07-03 16:26:08 +0300

    under construction sigh with Typed.js added

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,32 @@
+import Typed from "react-typed";
+
 function Home() {
-  return <div>Hello World!</div>;
+  return (
+    <div id="typed-strings">
+      <Typed
+        strings={[
+          "We're currently under construction",
+          "Come back after we are done!"
+        ]}
+        typeSpeed={100}
+        loop
+      />
+
+      <style jsx global>{`
+        body {
+          font-family: "Cinzel";
+          background: #ff2244;
+        }
+        #typed-strings {
+          width: auto;
+          font-size: 3em;
+          position: absolute;
+          top: 50%;
+          left: 50%;
+          transform: translateX(-50%);
+        }
+      `}</style>
+    </div>
+  );
 }
 export default Home;
